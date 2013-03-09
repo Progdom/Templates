1c5b021951aee0866a9ad3c24b433da11d3f7f13
Isaac Brown <isaacbrown@Isaacs-MacBook-Pro.local>
2013-03-08 17:51:10 -0800

    Initial Commit

---
+++ Templates/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Templates
+//
+//  Created by Isaac Brown on 3/8/13.
+//  Copyright (c) 2013 Isaac Brown. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
